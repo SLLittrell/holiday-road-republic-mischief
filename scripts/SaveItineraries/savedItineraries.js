30ff131c478e31263b79011843b9b9d10f8d830f
phillipphan <itsphillipphan@gmail.com>
2021-02-04 11:03:45 -0600

    save button saves object to local api

--- scripts/SaveItineraries/savedItineraries.js
+++ scripts/SaveItineraries/savedItineraries.js
@@ -1,5 +1,14 @@
+import { saveItineraries } from "./SaveDataProvider.js"
+
 const eventHub = document.querySelector("#container")
 const contentTarget = document.querySelector(".savedItinerary")
 
 eventHub.addEventListener("saveButtonClick", e => {
+  const savedItinerary = {
+    "savedPark": document.querySelector(".parkName").innerHTML,
+    "savedEatery": document.querySelector(".eateryContainer").innerHTML,
+    "savedAttraction": document.querySelector(".attractionContainer").innerHTML.trim()
+  }
+  
+  saveItineraries(savedItinerary)
 })
