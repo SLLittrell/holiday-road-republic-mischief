9940583ae37534bb40a267d25f2f21f485b534e6
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-04 15:54:35 -0600

    weather container css

--- scripts/weather/WeatherPreview.js
+++ scripts/weather/WeatherPreview.js
@@ -19,15 +19,15 @@ let parkState = ""
 const render= (weatherObject) => {
 const weatherhtmlRep = `
         <div class="weather">
-            <div>${parkCity}, ${parkState}<br>${weatherObject.map(dailyobj => {
-                return `${new Date(dailyobj.dt * 1000).toLocaleDateString("en-US")}<br>${dailyobj.weather.map(weatherobj => {
+            <div class="weather__cityState">${parkCity}, ${parkState}</div><div>${weatherObject.map(dailyobj => {
+                return `${new Date(dailyobj.dt * 1000).toLocaleDateString("en-US")}${dailyobj.weather.map(weatherobj => {
                     return `${weatherobj.description}`
-                })}<br>Feels like ${dailyobj.feels_like.day}&#8457<br>
+                })}Feels like ${dailyobj.feels_like.day}&#8457
                 Hi ${dailyobj.temp.max}&#8457 
-                Low ${dailyobj.temp.min}&#8457 <br>`
-            }).join(" ")}</div> 
-            </div>
-            `
+                Low ${dailyobj.temp.min}&#8457 `
+            }).join(" ")}<br></div> 
+        </div>
+        `
             weatherContainer.innerHTML = weatherhtmlRep
         }
         
