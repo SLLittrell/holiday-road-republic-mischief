a29115a94bc18dde7a88b83f7d6445bca07cf0c6
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-05 11:40:52 -0600

    Weather css change

--- scripts/weather/WeatherPreview.js
+++ scripts/weather/WeatherPreview.js
@@ -19,13 +19,17 @@ let parkState = ""
 const render= (weatherObject) => {
 const weatherhtmlRep = `
         <div class="weather">
-            <div class="weather__cityState">${parkCity}, ${parkState}</div><div>${weatherObject.map(dailyobj => {
-                return `${new Date(dailyobj.dt * 1000).toLocaleDateString("en-US")}${dailyobj.weather.map(weatherobj => {
-                    return `${weatherobj.description}`
-                })}Feels like ${dailyobj.feels_like.day}&#8457
-                Hi ${dailyobj.temp.max}&#8457 
-                Low ${dailyobj.temp.min}&#8457 `
-            }).join(" ")}<br></div> 
+            <div class="weather__cityState">${parkCity}, ${parkState}</div>
+            <div class="dailyWeather">${weatherObject.map(dailyobj => {
+                return `
+                   <div class="weatherDay"> ${new Date(dailyobj.dt * 1000).toLocaleDateString("en-US")}<br>
+                    ${dailyobj.weather.map(weatherobj => {
+                        return `${weatherobj.description}`})}
+                Feels like: ${Math.round(dailyobj.feels_like.day)}&#8457<br>
+                Hi: ${Math.round(dailyobj.temp.max)}&#8457 <br>
+                Low: ${Math.round(dailyobj.temp.min)}&#8457</div><br>`
+            }).join(" ")}
+            </div>
         </div>
         `
             weatherContainer.innerHTML = weatherhtmlRep
