eeabed82e52e561468667555206ed8d14e2cb992
phillipphan <itsphillipphan@gmail.com>
2021-02-09 09:24:17 -0600

    added comments

--- scripts/SaveItineraries/saveButton.js
+++ scripts/SaveItineraries/saveButton.js
@@ -1,12 +1,14 @@
 const contentTarget = document.querySelector(".saveItinerary")
 const eventHub = document.querySelector("#container")
 
+//renders save button
 export const saveButton = () => {
   contentTarget.innerHTML = `
     <button id="saveItinerary">Save Itinerary</button>
   `
 }
 
+//event listener for save button click
 eventHub.addEventListener("click", e => {
   if (e.target.id === "saveItinerary") {
     const customEvent = new CustomEvent("saveButtonClick")
